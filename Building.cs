74fd40f579334197247f99e757ea982fb6518ef5
naim90 <naim.elmoudni@gmail.com>
2021-09-30 11:02:16 +0200

    Classe building

---
+++ Building.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructeurs
+{
+    class Building
+    {
+
+		private double _hauteurBatiment;
+		private int _nombreEtages;
+
+		public Building(double hauteurBatiment, int nombreEtages)
+		{
+			_hauteurBatiment = hauteurBatiment;
+			_nombreEtages = nombreEtages;
+		}
+
+		public Building(int nombreEtages)
+		{
+			_hauteurBatiment =3* nombreEtages;
+		}
+
+
+		public double GetFloorMaxSize()
+        {
+			return _hauteurBatiment / _nombreEtages;
+        }
+
+		public double GetFloorCount()
+        {
+			return _nombreEtages;
+        }
+
+		public double GetSize()
+        {
+			return _hauteurBatiment;
+        }
+
+		/*		public Building(double etage)
+				{
+					hauteurBatiment = 3 * etage;
+				}
+		*/
+
+
+
+
+
+
+	}
+}
